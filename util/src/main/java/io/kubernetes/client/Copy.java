867d4abb6dc8e47507da5855d812919eb717101d
jamiecool <51167476+jamiecool@users.noreply.github.com>
2019-08-29 12:13:49 -0400

    Fix security bug

--- util/src/main/java/io/kubernetes/client/Copy.java
+++ util/src/main/java/io/kubernetes/client/Copy.java
@@ -125,7 +125,7 @@ public class Copy extends Exec {
           log.error("Can't read: " + entry);
           continue;
         }
-        File f = new File(destination.toFile(), entry.getName());
+/*        File f = new File(destination.toFile(), entry.getName());
         if (entry.isDirectory()) {
           if (!f.isDirectory() && !f.mkdirs()) {
             throw new IOException("create directory failed: " + f);
@@ -141,6 +141,7 @@ public class Copy extends Exec {
             fs.flush();
           }
         }
+*/
       }
     }
   }
